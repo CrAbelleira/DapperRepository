131897b0596f42860db4ba74879a540dccae7e38
Cristian Abelleira Olañeta <krisabol@gmail.com>
2019-04-27 23:33:51 +0200

    First ideas and DapperContext

---
+++ DapperRepository/DapperContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace DapperRepository
+{
+    public abstract class DapperContext
+    {
+        internal IDbConnection connection;
+        internal IDbTransaction transaction { get; set; }
+
+
+        public void SaveChanges()
+        {
+            transaction.Commit();
+        }
+
+        public DapperContext(IDbConnection connection)
+        {
+            this.connection = connection;
+        }
+        
+    }
+}
--- DapperRepository/DapperGenericRepository.cs
+++ DapperRepository/DapperGenericRepository.cs
@@ -11,42 +11,70 @@ namespace DapperRepository
     {
 
         private IDbConnection connection;
+        private IDbTransaction transaction;
 
         public void Delete(T entity)
         {
+            if(this.transaction== null)
+            {
+                this.transaction = connection.BeginTransaction();
+            }
             connection.Delete<T>(entity);
         }
 
         public T Get(long id)
         {
+            if (this.transaction == null)
+            {
+                this.transaction = connection.BeginTransaction();
+            }
             return connection.Get<T>(id);
         }
 
         public IQueryable<T> GetAll()
         {
+            if (this.transaction == null)
+            {
+                this.transaction = connection.BeginTransaction();
+            }
             return connection.GetAll<T>().AsQueryable<T>();
         }
 
         public IQueryable<T> GetBy(Func<T, bool> predicate)
         {
+            if (this.transaction == null)
+            {
+                this.transaction = connection.BeginTransaction();
+            }
             return connection.GetAll<T>().AsQueryable<T>().Where<T>(predicate).AsQueryable();
         }
 
         public long Insert(T entity)
         {
-           return connection.Insert<T>(entity);
+            if (this.transaction == null)
+            {
+                this.transaction = connection.BeginTransaction();
+            }
+            return connection.Insert<T>(entity);
         }
 
         public void Update(T entity)
         {
+            if (this.transaction == null)
+            {
+                this.transaction = connection.BeginTransaction();
+            }
             connection.Update<T>(entity);
         }
 
-        public DapperGenericRepository(IDbConnection connection)
+        public DapperGenericRepository(DapperContext context)
         {
-            this.connection = connection;
+            this.connection = context.connection;
+            this.transaction = context.transaction;           
         }
 
 
     }
 }
+
+  
