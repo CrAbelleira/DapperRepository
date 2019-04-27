5127dab5c24a84d4bb327a1de3b105073be17cda
Cristian Abelleira Olañeta <krisabol@gmail.com>
2019-04-27 20:11:41 +0200

    Agregar archivos de proyecto.

---
+++ DapperRepository/DapperGenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using DapperRepository.Interfaces;
+using System;
+using System.Data;
+using System.Linq;
+
+namespace DapperRepository
+{
+    public class DapperGenericRepository<T> : IRepository<T> where T : class
+    {
+
+        private IDbConnection connection;
+
+        public void Delete(T entity)
+        {
+            connection.Delete<T>(entity);
+        }
+
+        public T Get(long id)
+        {
+            return connection.Get<T>(id);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return connection.GetAll<T>().AsQueryable<T>();
+        }
+
+        public IQueryable<T> GetBy(Func<T, bool> predicate)
+        {
+            return connection.GetAll<T>().AsQueryable<T>().Where<T>(predicate).AsQueryable();
+        }
+
+        public long Insert(T entity)
+        {
+           return connection.Insert<T>(entity);
+        }
+
+        public void Update(T entity)
+        {
+            connection.Update<T>(entity);
+        }
+
+        public DapperGenericRepository(IDbConnection connection)
+        {
+            this.connection = connection;
+        }
+
+
+    }
+}
---
+++ DapperRepository/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DapperRepository.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(long id);
+        IQueryable<T> GetAll();
+        IQueryable<T> GetBy(Func<T,bool> predicate);
+        long Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
