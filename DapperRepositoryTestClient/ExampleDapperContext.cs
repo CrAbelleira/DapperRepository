ee217c39dc74dd0e90a03018b0d4c83feee96d37
Cristian Abelleira Olañeta <krisabol@gmail.com>
2019-04-27 23:34:24 +0200

    Test client project

---
+++ DapperRepositoryTestClient/ExampleDapperContext.cs
@@ -0,0 +1,24 @@
+﻿using DapperRepository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DapperRepositoryTestClient
+{
+    class ExampleDapperContext : DapperContext
+    {
+
+        public DapperGenericRepository<String> tblString { get; }
+        public DapperGenericRepository<String> tblString2 { get; }
+        public DapperGenericRepository<String> tblString3 { get; }
+        public DapperGenericRepository<String> tblString4 { get; }
+
+
+        ExampleDapperContext(IDbConnection connection) : base(connection)
+        {
+            this.tblString = new DapperGenericRepository<string>(this);
+            
+        }
+    }
+}
